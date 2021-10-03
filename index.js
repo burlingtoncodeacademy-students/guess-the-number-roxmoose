5ccb04eda26ca7034b61d68fd35c5404b76249d1
Roxanne Meuse <roxannemeuse@Roxannes-Air.lan>
2021-10-03 13:29:28 -0400

    trying to get the reverse game on git again

--- index.js
+++ index.js
@@ -20,23 +20,25 @@ async function start() {
     "Please enter a number that we'll use as the highest possible guess => ");
     highest = parseInt(highest); 
 
-  let secretNumber = await ask("Ok, now enter a secret numer between 1 and " + highest + ". (I promise I won't look.)\n");
+  let secretNumber = await ask("Ok, now enter a secret number between 1 and " + (highest) + ". (I promise I won't look.)\n");
   console.log("YOUR SECRET NUMBER: " + secretNumber);
 
   // Story 1: "Pick a Number, Any Number"
 
   function randomNum() {
-    let range = highest - lowest + 1;
-    return Math.floor(Math.random() * range) + lowest;
+    return Math.floor(Math.random() * highest + lowest )
   }
 
   let guess = randomNum();
+
+  console.log("high range is " + highest); // seeing what the high range is. delete later
 
   let response = await ask("Is your number " + guess + "? yes or no? => ");
 
   // Story 2: Computer guessed correctly
   if (response === "yes") {
     console.log("Wow there was a 1 in 100 chance of that. Let's go eat cake!");
+    process.exit()
   }
 
   // Story 3: Computer guessed wrong
@@ -44,11 +46,12 @@ async function start() {
     response = await ask(
       "I'll try again! Is your number higher (key in 'H') or lower (key in 'L')? => "
     );
+    let calledTimes = 1; // ICEBOX - counting number of guesses
 
     // Stories 4 and 5: "Modify Your Guess Range" and "Make it Smarter"
 
     function guessHigher() {
-      guess = Math.floor((highest + guess) / 2);
+      guess = Math.floor((highest + 1 + guess) / 2); // adding 1 so range actually goes up to AND INCLUDES their high range
       return guess;
     }
 
@@ -57,28 +60,44 @@ async function start() {
       return guess;
     }
 
-    while (response !== "yes") {
-      if (response === "H") {
-        // console.log(guess, highest) // just checking to make sure it's using the correct range
+    while (response !== "yes" && response !== "Yes" && response !== "YES") {
+
+      if (response === "H" || response === "h") {
         lowest = guess;
         response = await ask(
           "Higher you say? Ok. Is your number " +
             guessHigher() +
-            "? \nKey in 'yes' if it is, or 'H' or 'L' for me to guess higher or lower! => "
+            "? \nKey in 'yes' if it is, or 'h' or 'l' for me to guess higher or lower! => "
         );
+
+        calledTimes = calledTimes += 1 // For ICEBOX 2: How many tries
+
       } else {
-        // console.log(lowest, guess) // just checking to make sure it's using the correct range
         highest = guess;
         response = await ask(
           "Lower you say? Ok. Is your number " +
             guessLower() +
-            "? \nKey in 'yes' if it is, or 'H' or 'L' for me to guess higher or lower! => "
+            "? \nKey in 'yes' if it is, or 'h' or 'l' for me to guess higher or lower! => "
         );
+
+        calledTimes = calledTimes += 1 // For ICEBOX 2: How many tries
+
       }
     }
     console.log("Yay!");
-    process.exit();
+    console.log("It took me " + calledTimes + " guesses.") // for ICEBOX 2: How many tries
+
+    //For ICEBOX 1: play again (but doesn't work yet):
+    let playAgain = await ask("Would you like to play again? Key in 'yes' or 'no' => ")
+    if (playAgain === "yes") {
+      start()
+    } else {
+      console.log("Thanks for playing! Byeeee")
+      process.exit()
+    }
+
   }
 
-  process.exit();
+  process.exit()
+
 }
---
+++ reverse_game.js
@@ -0,0 +1,41 @@
+const readline = require("readline");
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+function ask(questionText) {
+    return new Promise((resolve, reject) => {
+        rl.question(questionText, resolve);
+    });
+}
+
+start();
+
+async function start() {
+    console.log("Let's play a game where I (computer) think of a random number and you (human) try to guess it. The lowest possible number is 1. ")
+
+    let lowest = 1;
+    let highest = await ask("Please enter a number that we'll use as the highest possible number => ")
+    highest = parseInt(highest)
+
+    let secretNumber = Math.floor(Math.random() * (highest - lowest)) + lowest
+    console.log(secretNumber) // just checking; remove later
+
+    console.log("Ok. I'm thinking of a number between 1 and " + highest + ".")
+
+    let guess = await ask("What is your guess? => ")
+    guess = parseInt(guess)
+  
+    while (guess !== secretNumber) {
+     if (guess > secretNumber) {
+            guess = await ask("Too high! Guess lower => ")
+            guess = parseInt(guess)
+        } else {
+            guess = await ask("Too low! Guess higher => ")
+            guess = parseInt(guess)
+        }
+
+    }
+
+    console.log("You got it! My number was indeed " + secretNumber + "! Treat yourself to some cake.")
+    process.exit()
+
+}
